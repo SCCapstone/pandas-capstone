060d5046be047d520df07847978e70c0240b7003
Kelly Finnegan <finnegak@email.sc.edu>
2024-10-25 14:40:56 -0400

    code

--- pandas-capstone/src/App.js
+++ pandas-capstone/src/App.js
@@ -6,11 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Pandas Capstone Project</h1>
-<<<<<<< HEAD
         <h2>LearnLink</h2>
-        <h3>Trying to conflict</h3>
-=======
-        <h2>Merge conflict - kelly </h2>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -24,7 +21,6 @@ function App() {
         </a>
         <h2>LearnLink</h2>
         <h2>file</h2>
->>>>>>> bf968668263cb9d9f033e176e2ddaffcf8b092d9
       </header>
     </div>
   );
