083c0ba844a2d7c0230000825fee57b182160b23
Kelly Finnegan <finnegak@email.sc.edu>
2024-10-25 14:26:52 -0400

    merge conflict

--- pandas-capstone/src/App.js
+++ pandas-capstone/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Pandas Capstone Project</h1>
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Merge conflict - kelly </h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
