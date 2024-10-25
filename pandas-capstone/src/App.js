5e52084ff7def7d87c573e5867fadcb6777b1d6e
Kelly Finnegan <finnegak@email.sc.edu>
2024-10-25 14:34:33 -0400

    push

--- pandas-capstone/src/App.js
+++ pandas-capstone/src/App.js
@@ -6,22 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Pandas Capstone Project</h1>
-<<<<<<< Updated upstream
-        <h2>Merge conflict - kelly </h2>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-=======
         <h2>LearnLink</h2>
->>>>>>> Stashed changes
+        <h3>Trying to conflict</h3>
       </header>
     </div>
   );
