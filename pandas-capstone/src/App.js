2239de2b1d5e045c12f1d035900c0b64e41159f3
Klutzgurl477 <yppatel@email.sc.edu>
2024-10-26 13:03:48 -0400

    clean code

--- pandas-capstone/src/App.js
+++ pandas-capstone/src/App.js
@@ -6,21 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Pandas Capstone Project</h1>
-        <h2>Kelly</h2>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link!!!!!!"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h2>LearnLinkkkkk</h2>
-        <h2>file</h2>
-        <h3>hi</h3>
+        <h2>LearnLink</h2>
       </header>
     </div>
   );
