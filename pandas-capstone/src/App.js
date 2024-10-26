99b62086b3d9b232a7d81c27403215053f45166b
Klutzgurl477 <yppatel@email.sc.edu>
2024-10-26 15:45:18 -0400

    login page

--- pandas-capstone/src/App.js
+++ pandas-capstone/src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Pandas Capstone Project</h1>
-        <h2>LearnLink</h2>
-      </header>
+        <h1>Welcome!</h1>
+        <p>LearnLink is an online platform that<br></br>connects like-minded individuals to <br></br>form study groups!</p>
+        <button className="getStarted">Get Started</button> <br></br>
+
+        <button className="studyGroup">Study Groups</button> <br></br>
+        <button className="studyResources">Study Resources</button> <br></br>
+        <button className="messaging">Messaging</button> <br></br>
+        <button className="gradeCalculator">Grade Calculator</button> 
+        <footer><p className="copyright">&copy; LearnLink</p></footer>
     </div>
   );
 }
