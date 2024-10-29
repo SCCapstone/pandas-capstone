cb1aa024fdc7a431a293309056695b15899cc020
nataliecrawford <crawfon@email.sc.edu>
2024-10-29 12:30:53 -0400

    adding logo and fixing css for logo and copyright

--- learnlink-ui/src/App.tsx
+++ learnlink-ui/src/App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ReactComponent as MySvgFile } from './LearnLink.svg';
 
 function App() {
   return (
     <div className="App">
+      <div className = "Logo">
+        <MySvgFile />
+      </div>
+      <div className = "WelcomePage">
         <h1>Welcome!</h1>
         <p>LearnLink is an online platform that<br></br>connects like-minded individuals to <br></br>form study groups!</p>
         <button className="getStarted">Get Started</button> <br></br>
-
         <button className="studyGroup">Study Groups</button> <br></br>
         <button className="studyResources">Study Resources</button> <br></br>
         <button className="messaging">Messaging</button> <br></br>
         <button className="gradeCalculator">Grade Calculator</button> 
-        <footer><p className="copyright">&copy; LearnLink</p></footer>
+      </div>
+      <div className = "Copyright">
+        <footer>&copy; LearnLink</footer>
+      </div>
     </div>
   );
 }
