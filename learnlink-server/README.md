c54f30cc412f953fffe9ec6753ac5c2ce572dc3a
nataliecrawford <crawfon@email.sc.edu>
2024-10-29 11:00:04 -0400

    separating into ui and server

---
+++ learnlink-server/README.md
@@ -0,0 +1,5 @@
+# Install dependencies commands
+type these commands in the folder of server in the command line
+-- npm i ts-node typescript nodemon @types/cors @types/express @types/node --save-dev
+-- npm i @prisma/client cors express prisma
+-- npx tsc --init
---
+++ learnlink-server/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+//import { PrismaClient } from "@prisma/client";
+
+ 
+const app = express();
+//const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(2020, () => {
+    console.log("server running on localhost:2020");
+  });
