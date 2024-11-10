5ded2f0e4c8b59531e1bed2d2bf5e0d1377ea9f6
Kelly Finnegan <finnegak@email.sc.edu>
2024-11-10 16:15:48 -0500

    added more signup questions and css to signup page

--- learnlink-ui/src/pages/signup.tsx
+++ learnlink-ui/src/pages/signup.tsx
@@ -10,19 +10,33 @@ function Signup() {
             </div>
             <div className="container">
                 <h1 className="l1">Sign Up</h1>
-                <h2 className="t2">Create an account to get started.</h2>
+                <h2 className="t2">Enter your credentials to join LearnLink.</h2>
                 
-                <label>Full Name</label>
-                <input type="text" placeholder="John Doe"></input>
+                {/* First and Last Name Inputs */}
+                <div className="nameFields">
+                    <label>First Name</label>
+                    <input type="text" placeholder="John" />
+                </div>
+                <div className="nameFields">
+                    <label>Last Name</label>
+                    <input type="text" placeholder="Doe" />
+                </div>
                 
+                {/* Username Input */}
+                <label>Username</label>
+                <input type="text" placeholder="john_doe123" />
+                
+                {/* Email Input */}
                 <label>Email</label>
-                <input type="text" placeholder="example@learnlink.com"></input>
+                <input type="text" placeholder="example@learnlink.com" />
                 
+                {/* Password Input */}
                 <label>Password</label>
-                <input type="password" placeholder="**************"></input>
-
+                <input type="password" placeholder="**************" />
+                
+                {/* Confirm Password Input */}
                 <label>Confirm Password</label>
-                <input type="password" placeholder="**************"></input>
+                <input type="password" placeholder="**************" />
                 
                 <button className="signUpButton">Sign Up</button>
 
