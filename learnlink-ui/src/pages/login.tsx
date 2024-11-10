d10d40383917615c30288cb0b179002daa5c742c
Klutzgurl477 <yppatel@email.sc.edu>
2024-11-10 14:29:35 -0500

    login css

--- learnlink-ui/src/pages/login.tsx
+++ learnlink-ui/src/pages/login.tsx
@@ -1,35 +1,40 @@
-
+import './login.css';
 import Logo from '../components/Logo';
+import Copyright from '../components/CopyrightFooter';
 
 function Login() {
     return (
         <div className="login">
-        <div className = "Logo">
+        <div className = "Logo2">
             <Logo />
         </div>
         <div className="container">
-           <h1>Login</h1>
-            <h2>Enter your credentials to login.</h2>
+           <h1 className="l1">Login</h1>
+           <h2 className="t2">Enter your credentials to login.</h2>
             <label>
                 Email
-                <br></br>
+                </label>
                 <input type="text" placeholder="example@learnlink.com"></input>
-            </label>
-            <br></br>
             <label>
                 Password
-                <br></br>
-                <input type="text" placeholder="**************"></input>
-                </label>
-                <div>
+            </label>
+            <input type="text" placeholder="**************"></input>
+            <div className="fp">
                 <label>Forgot password?</label>   
-                </div>
-                <button className="lButton">Login</button>
+            </div>
+
+            <button className="lButton">Login</button>
+
+            <div className="or">
                 <label>OR</label>
-                <button className="sign">Sign Up</button> 
+            </div>
+                
+            <button className="sign">Sign Up</button> 
         </div>
-        
-        
+
+        <div className = "Copyright2">
+          <Copyright/>
+        </div>
 
         </div>
     );
