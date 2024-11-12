7ef569897295d53033ed80206fdaffed241d714a
Kennedy <kenbhx@gmail.com>
2024-11-12 13:14:57 -0500

    placeholder

---
+++ learnlink-ui/src/pages/profile.tsx
@@ -0,0 +1,20 @@
+// placeholder
+
+import React from 'react';
+import Navbar from '../components/Navbar';
+import './LandingPage.css';
+import CopyrightFooter from '../components/CopyrightFooter';
+
+const LandingPage: React.FC = () => {
+  return (
+    <div className="landing-page">
+      <Navbar />
+      <main className="content">
+        <p>To start searching please update your profile via top left corner!</p>
+      </main>
+      <CopyrightFooter />
+    </div>
+  );
+};
+
+export default LandingPage;
