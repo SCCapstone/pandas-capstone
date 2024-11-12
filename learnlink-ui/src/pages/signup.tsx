26cdb58b4e926058b70d7694bd063f1c8c738857
Kelly Finnegan <finnegak@email.sc.edu>
2024-11-12 10:51:38 -0500

    signup now goes to landing page

--- learnlink-ui/src/pages/signup.tsx
+++ learnlink-ui/src/pages/signup.tsx
@@ -1,8 +1,16 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './signup.css';
 import Logo from '../components/Logo';
 import Copyright from '../components/CopyrightFooter';
 
-function Signup() {
+const Signup: React.FC = () => {
+    const navigate = useNavigate();
+
+    const handleSignup = () => {
+        navigate('/LandingPage');
+    };
+
     return (
         <div className="signup">
             <div className="Logo2">
@@ -12,7 +20,6 @@ function Signup() {
                 <h1 className="l1">Sign Up</h1>
                 <h2 className="t2">Enter your credentials to join LearnLink.</h2>
                 
-                {/* First and Last Name Inputs */}
                 <div className="nameFields">
                     <label>First Name</label>
                     <input type="text" placeholder="John" />
@@ -22,24 +29,20 @@ function Signup() {
                     <input type="text" placeholder="Doe" />
                 </div>
                 
-                {/* Username Input */}
                 <label>Username</label>
                 <input type="text" placeholder="john_doe123" />
                 
-                {/* Email Input */}
                 <label>Email</label>
                 <input type="text" placeholder="example@learnlink.com" />
                 
-                {/* Password Input */}
                 <label>Password</label>
                 <input type="password" placeholder="**************" />
                 
-                {/* Confirm Password Input */}
                 <label>Confirm Password</label>
                 <input type="password" placeholder="**************" />
                 
-                <button className="signUpButton">Sign Up</button>
-
+                <button className="signUpButton" onClick={handleSignup}>Sign Up</button>
+                
                 <div className="loginRedirect">
                     <label>Already have an account? <a href="/login">Log in</a></label>
                 </div>
