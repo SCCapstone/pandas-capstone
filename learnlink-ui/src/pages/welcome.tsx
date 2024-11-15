87e23214c920dc841a0c78131f5ca1cb74bc7e20
raej15 <sej15@email.sc.edu>
2024-11-14 22:05:28 -0500

    restyled welcom page

--- learnlink-ui/src/pages/welcome.tsx
+++ learnlink-ui/src/pages/welcome.tsx
@@ -13,19 +13,25 @@ function Welcome() {
 
   return (
       <div className="welcome">
-      <div className = "WelcomeLogo">
-        <Logo/>
+      <div className="Logo2">
+                <Logo />
       </div>
       <div className = "WelcomePage">
-        <h1 className="well">Welcome!</h1>
-        <p>LearnLink is an online platform that<br></br>connects like-minded individuals to <br></br>form study groups!</p>
-        <button className="getStarted" onClick={handleGetStarted}>Get Started</button> <br></br>
-        <button className="studyGroup">Study Groups</button> <br></br>
-        <button className="studyResources">Study Resources</button> <br></br>
-        <button className="messaging">Messaging</button> <br></br>
-        <button className="gradeCalculator">Grade Calculator</button> 
+        <div className='leftContainer'>
+          <div className='titleContainer'>
+            <h1 className="well">Welcome!</h1>
+            <p>LearnLink is an online platform that connects like-minded individuals to form study groups!</p>
+          </div>
+          <button className="getStarted" onClick={handleGetStarted}>Get<br></br>Started</button>
+        </div>
+        <div className='menuButtonContainer'>
+          <button className="menuButtons">Study Groups</button> <br></br>
+          <button className="menuButtons">Study Resources</button> <br></br>
+          <button className="menuButtons">Messaging</button> <br></br>
+          <button className="menuButtons">Grade Calculator</button> 
+        </div>
       </div>
-        <div className = "Copyright">
+        <div className = "Copyright2">
           <Copyright/>
         </div>
       </div>
