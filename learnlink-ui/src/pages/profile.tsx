6e6c583dd88d7d0c79b8158d11cc486caf73be06
raej15 <sej15@email.sc.edu>
2024-11-19 13:42:33 -0500

    updated profile page styling

--- learnlink-ui/src/pages/profile.tsx
+++ learnlink-ui/src/pages/profile.tsx
@@ -7,46 +7,51 @@ const Profile: React.FC = () => {
   return (
     <div className="profile-page">
       <Navbar />
-      <header className="profile-header">
-        <h1 className="profile-title"></h1>
-      </header>
-      <main className="profile-content">
-        <div className="profile-details">
-          <form className="profile-form">
-            <label>
-              Age: <input type="text" name="age" />
-            </label>
-            <label>
-              College: <input type="text" name="college" />
-            </label>
-            <label>
-              Major: <input type="text" name="major" />
-            </label>
-            <label>
-              Grade (undergrad/grad): <input type="text" name="grade" />
-            </label>
-            <label>
-              Relevant Course: <input type="text" name="course" />
-            </label>
-            <label>
-              Fav Study Method: <input type="text" name="studyMethod" />
-            </label>
-            <label>
-              Bio: <textarea name="bio" />
-            </label>
-            <label>
-              Gender: <input type="text" name="gender" />
-            </label>
-          </form>
-          <div className="profile-picture">
-            <button className="upload-button">CLICK TO ADD PICTURE</button>
+        <div className='main-container'>
+        <header className="profile-header">
+          <h1 className="profile-title">Update Profile</h1>
+        </header>
+        <main className="profile-content">
+          <div className="profile-details">
+            <form className="profile-form">
+              <label>
+                Age: <input type="text" name="age" />
+              </label>
+              <label>
+                College: <input type="text" name="college" />
+              </label>
+              <label>
+                Major: <input type="text" name="major" />
+              </label>
+              <label>
+                Grade (undergrad/grad): <input type="text" name="grade" />
+              </label>
+              <label>
+                Relevant Course: <input type="text" name="course" />
+              </label>
+              <label>
+                Fav Study Method: <input type="text" name="studyMethod" />
+              </label>
+              <label>
+                Gender: <input type="text" name="gender" />
+              </label>
+            </form>
+            
+              <form className="profile-form">
+                <div className="profile-picture">
+                  <button className="upload-button">CLICK TO ADD PICTURE</button>
+                </div>
+                <label>
+                    Bio: <textarea name="bio" />
+                </label>          
+                <div className="profile-buttons">
+                  <button className="back-button">BACK</button>
+                  <button className="save-button">SAVE</button>
+                </div>
+              </form>
           </div>
-        </div>
-        <div className="profile-buttons">
-          <button className="back-button">BACK</button>
-          <button className="save-button">SAVE</button>
-        </div>
-      </main>
+        </main>
+      </div>
       <CopyrightFooter />
     </div>
   );
