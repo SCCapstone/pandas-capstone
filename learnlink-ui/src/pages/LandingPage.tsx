c3c54c8bd4cc1276fcc16d2b070cf3a70d4b4b43
raej15 <sej15@email.sc.edu>
2024-11-20 12:44:52 -0500

    fixed landing page centering

--- learnlink-ui/src/pages/LandingPage.tsx
+++ learnlink-ui/src/pages/LandingPage.tsx
@@ -7,12 +7,14 @@ import CopyrightFooter from '../components/CopyrightFooter';
 const LandingPage: React.FC = () => {
   return (
     <div className="landing-page">
+      <div>
       <Navbar />
+      </div>
       <main className="content">
         <p>To start searching please update your profile via top left corner!</p>
       </main>
       <div>
-      <CopyrightFooter />
+        <CopyrightFooter />
       </div>
     </div>
   );
