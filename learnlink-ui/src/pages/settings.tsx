8fa88bc7c74ccef2b36c874eb0eb50701b808eda
raej15 <sej15@email.sc.edu>
2024-11-19 20:38:00 -0500

    implemented logout button

--- learnlink-ui/src/pages/settings.tsx
+++ learnlink-ui/src/pages/settings.tsx
@@ -3,6 +3,8 @@ import Navbar from '../components/Navbar';
 import './settings.css'
 import CopyrightFooter from '../components/CopyrightFooter';
 import { useNavigate } from 'react-router-dom';
+import { logout } from '../utils/auth';
+
 
 const Settings: React.FC = () => {
   const navigate = useNavigate();
@@ -14,7 +16,7 @@ const Settings: React.FC = () => {
     <div className="settings">
       <Navbar />
       <div className="content">
-        <button className="logOut" onClick={handleLogOut}>Log Out</button>
+        <button className="logOut" onClick={logout}>Log Out</button>
         <button className="changePassword">Change Password</button>
         <button className="deleteAccount">Delete Account</button>
       </div>
--- learnlink-ui/src/utils/auth.tsx
+++ learnlink-ui/src/utils/auth.tsx
@@ -1,1 +1,6 @@
-export {}
+export const logout = () => {
+    localStorage.removeItem('token'); // Remove the JWT from localStorage
+    // If you have other user data stored, clear it as well
+    localStorage.removeItem('user');
+    window.location.href = '/login'; // Redirect to login page
+  };
