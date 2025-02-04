1eef2328d91f9982e4b83ae3a9f08ef320dfc247
nataliecrawford <crawfon@email.sc.edu>
2025-02-04 13:30:56 -0500

    updating resources to include real info

--- learnlink-ui/src/pages/resources/studyTips.tsx
+++ learnlink-ui/src/pages/resources/studyTips.tsx
@@ -18,18 +18,17 @@ const StudyTips: React.FC = () => {
           <h1>Study Tips</h1>
           <div className="tip-icon">
           </div>
-          <h2>Tip #1</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo neque id sapien dignissim euismod. Fusce sagittis dignissim lorem ac mattis. Vestibulum non ipsum dui. Proin nec mi consectetur ante sodales accumsan. Nullam consequat tellus vel rutrum blandit.</p>
+          <h2>Collaborate Asynchronously</h2>
+          <p>Create shared Quizlet study cards or have a shared Notes page, so that multiple people can input their takeaways from lectures.</p>
 
-          <h2>Tip #2</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo neque id sapien dignissim euismod. Fusce sagittis dignissim lorem ac mattis. Vestibulum non ipsum dui. Proin nec mi consectetur ante sodales accumsan. Nullam consequat tellus vel rutrum blandit.</p>
-
-          <h2>Tip #3</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo neque id sapien dignissim euismod. Fusce sagittis dignissim lorem ac mattis. Vestibulum non ipsum dui. Proin nec mi consectetur ante sodales accumsan. Nullam consequat tellus vel rutrum blandit.</p>
-
-          <h2>Tip #4</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo neque id sapien dignissim euismod. Fusce sagittis dignissim lorem ac mattis. Vestibulum non ipsum dui. Proin nec mi consectetur ante sodales accumsan. Nullam consequat tellus vel rutrum blandit.</p>
-
+          <h2>Choose Study Location Wisely</h2>
+          <p>Ensure that the needs of each group member is satisfied in the location of the meeting spot. If most members prefer someplace quiet, choose something like the library, whereas if the group prefers a lively background, maybe choose a coffee shop.</p>
+          
+          <h2>Teach Eachother</h2>
+          <p>Take turns explaining the content from memory without looking at notes to eachother, reviewing the missed content afterwards</p>
+          
+          <h2>Dumb It Down</h2>
+          <p>When studying attempt to explain it to eachother as if the other person does not know the content. Learn how to take high-level content to a elementary level to better your own understanding of the material.</p>
         </main>
       </div>
       <footer>
@@ -40,3 +39,5 @@ const StudyTips: React.FC = () => {
 };
 
 export default StudyTips;
+
+
