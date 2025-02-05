9a8a40d420b1915cae12136cd9e23efdd39b60ac
raej15 <sej15@email.sc.edu>
2025-02-04 22:26:14 -0500

    something wrong woth study group creation now?

--- learnlink-ui/src/components/SwipeProfiles.tsx
+++ learnlink-ui/src/components/SwipeProfiles.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import './SwipeProfiles.css';
 import { formatEnum } from '../utils/format';
+import { ReactComponent as GroupLogo } from './GroupLogo.svg'
+
 
 const SwipeProfiles = ({ userId }: { userId: number }) => {
   const [profiles, setProfiles] = useState<any>({ users: [], studyGroups: [] });
@@ -78,152 +80,155 @@ const SwipeProfiles = ({ userId }: { userId: number }) => {
       {currentProfile ? (
         <div className="profile-card">
           {currentProfile.chatID ? (
-            <div className="swipe-main-container">
+            <div className="group-container">
 
-              <div className="swipe-left-side" style={{width: '100%'}}>
-                <h1>{currentProfile.name}</h1>
+            <h1 className='group-title'>Study Group: {currentProfile.name}</h1>
 
-                <div className="bio">
-                  <h3>Bio:</h3>
-                  <p>{currentProfile.description}</p>
-                  <p><span className="bold-first-word">Course: </span>{currentProfile.subject}</p>
+            <div className="group-info">
+              
+
+              <div className="group-right">
+                <GroupLogo/>
+                <div className="group-description">
+                <span className="bold-first-word">Bio: </span><p>{currentProfile.subject}</p>
+                </div>
+                <div className='group-subject'>
+                  <span className="bold-first-word">Course: </span><p>{currentProfile.subject}</p>
+                  </div>
 
                 </div>
-              </div>
-              <div className="swipe-right-side">
-                <h3>Members:</h3>
-                <div className="study-group-members">
-                  {currentProfile.users && currentProfile.users.length > 0 ? (
-                    currentProfile.users.map((member: any, index: number) => (
-                      <div key={index} className="profile-card">
-                        <div className='swipe-main-container'>
-                        <h1>{member.name}</h1>
-                        <div className="swipe-left-side" >
-                          <img
-                            src={member.profilePic || 'https://learnlink-public.s3.us-east-2.amazonaws.com/AvatarPlaceholder.svg'}
-                            alt={`${member.firstName} ${member.lastName}` || member.name}
-                            className="profile-pic"
-                          />
-                          <div className="bio">
-                            <h3>Bio:</h3>
-                            <p>{member.bio}</p>
+                <div className="group-left">
+                  {/* <h1>Members:</h1> */}
+                  <div className="member-cards">
+                    {currentProfile.users && currentProfile.users.length > 0 ? (
+                      currentProfile.users.map((member: any, index: number) => (
+                        <div key={index} className="member-card">
+                          <div className="member-card-top">
+                            <h1>{member.name}</h1>
+                            <div className="member-card-top-left" >
+                              <img
+                                src={member.profilePic || 'https://learnlink-public.s3.us-east-2.amazonaws.com/AvatarPlaceholder.svg'}
+                                alt={`${member.firstName} ${member.lastName}`}
+                                className="member-pic"
+                              />
+                            </div>
+                            <div className="member-card-top-right">
+                            <h1>{member.firstName} {member.lastName}</h1>
+                            <h2>@{member.username}</h2>
+                            </div>
                           </div>
-                        </div>
-                        <div className="swipe-right-side">
-                          <h1 style={{ fontSize: '2rem' }}>{member.firstName} {member.lastName}</h1>
-                          <h3>@{member.username}</h3>
-                          <div className="profile-details-container">
-                            {/* <div className="swipe-profile-details">
+                          <>
+                              <p><span className="bold-first-word">Bio:</span></p>
+                              <p>{member.bio}</p>
+
+                              {/* <div className="swipe-profile-details">
                               <p><span className="bold-first-word">Age: </span>{member.age}</p>
                               <p><span className="bold-first-word">College: </span>{member.college}</p>
                               <p><span className="bold-first-word">Major: </span>{member.major}</p>
                               <p><span className="bold-first-word">Gender: </span>{member.gender}</p>
                               </div> */}
-                              <div className="swipe-profile-details">
-{/* 
+                                {/* 
                               <p><span className="bold-first-word">Grade: </span>{member.grade}</p>
                               <p><span className="bold-first-word">Relevant Coursework: </span>{member.relevant_courses}</p>
                               <p><span className="bold-first-word">Fav Study Method: </span>{member.study_method}</p>
                               */}
-                              <p><span className="bold-first-word">Study Tags: </span>
-                                {member.studyHabitTags.length > 0 ? (
-                                  member.studyHabitTags.map((tag: string, index: number) => (
-                                    <span key={index} className="tag">
-                                      {formatEnum(tag)}
-                                    </span>
-                                  ))
-                                ) : (
-                                  "No study tags specified."
-                                )}
-                              </p>
-                            </div>
-                          </div>
+                                <p><span className="bold-first-word">Study Tags: <br></br></span>
+                                  {member.studyHabitTags.length > 0 ? (
+                                    member.studyHabitTags.map((tag: string, index: number) => (
+                                      <span key={index} className="member-tag">
+                                        {formatEnum(tag)}
+                                      </span>
+                                    ))
+                                  ) : (
+                                    "No study tags specified."
+                                  )}
+                                </p>
+                          </>
                         </div>
-                      </div>
-                      </div>
-                    ))
-                  ) : (
-                    <p>No members yet.</p>
-                  )}
+                      ))
+                    ) : (
+                      <p>No members yet.</p>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
 
-              ) : (
-              <div className="swipe-main-container">
-
-                <>
-                  <h1>{currentProfile.name}</h1>
-                  <div className="swipe-left-side">
-                    <img
-                      src={currentProfile.profilePic}
-                      alt={`${currentProfile.firstName} ${currentProfile.lastName}` || currentProfile.name}
-                      className="profile-pic"
-                    />
-                    <div className="bio">
-                      <h3>Bio:</h3>
-                      <p>{currentProfile.bio}</p>
-                    </div>
-                  </div>
-                  <div className="swipe-right-side">
-                    <h1>{currentProfile.firstName} {currentProfile.lastName}</h1>
-                    <h3>@{currentProfile.username}</h3>
-                    <div className="profile-details-container">
-                      <div className="swipe-profile-details">
-                        <p><span className="bold-first-word">Age: </span>{currentProfile.age}</p>
-                        <p><span className="bold-first-word">College: </span>{currentProfile.college}</p>
-                        <p><span className="bold-first-word">Major: </span>{currentProfile.major}</p>
-                        <p><span className="bold-first-word">Gender: </span>{currentProfile.gender}</p>
-                      </div>
-                      <div className="swipe-profile-details">
-                        <p><span className="bold-first-word">Grade: </span>{currentProfile.grade}</p>
-                        <p><span className="bold-first-word">Relevant Coursework: </span>{currentProfile.relevant_courses}</p>
-                        <p><span className="bold-first-word">Fav Study Method: </span>{currentProfile.study_method}</p>
-                        <p><span className="bold-first-word">Study Tags: </span>
-                          {currentProfile.studyHabitTags.length > 0 ? (
-                            currentProfile.studyHabitTags.map((tag: string, index: number) => (
-                              <span key={index} className="tag">
-                                {formatEnum(tag)}
-                              </span>
-                            ))
-                          ) : (
-                            "No study tags specified."
-                          )}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </>
-              </div>
-              )}
-
-              <div className="swipe-buttons-container">
-                <button
-                  onClick={handleBack}
-                  disabled={currentProfileIndex === 0}
-                  style={{ visibility: currentProfileIndex > 0 ? "visible" : "hidden" }}
-                >
-                  Back
-                </button>
-
-                <div className="swipe-action-buttons">
-                  <button onClick={() => { handleSwipe("Yes", currentProfile.id, !!currentProfile.studyGroupId); handleMessaging(); }}>
-                    Match
-                  </button>
-
-                  <button onClick={() => handleSwipe("No", currentProfile.id, !!currentProfile.studyGroupId)}>
-                    Skip
-                  </button>
-                </div>
-              </div>
-            </div>
           ) : (
-            <div className="swipe-info">
-              <p>No more profiles to swipe on!</p>
+            <div className="swipe-main-container">
+
+              <>
+                <h1>{currentProfile.name}</h1>
+                <div className="swipe-left-side">
+                  <img
+                    src={currentProfile.profilePic}
+                    alt={`${currentProfile.firstName} ${currentProfile.lastName}` || currentProfile.name}
+                    className="profile-pic"
+                  />
+                  <div className="bio">
+                    <h3>Bio:</h3>
+                    <p>{currentProfile.bio}</p>
+                  </div>
+                </div>
+                <div className="swipe-right-side">
+                  <h1>{currentProfile.firstName} {currentProfile.lastName}</h1>
+                  <h3>@{currentProfile.username}</h3>
+                  <div className="profile-details-container">
+                    <div className="swipe-profile-details">
+                      <p><span className="bold-first-word">Age: </span>{currentProfile.age}</p>
+                      <p><span className="bold-first-word">College: </span>{currentProfile.college}</p>
+                      <p><span className="bold-first-word">Major: </span>{currentProfile.major}</p>
+                      <p><span className="bold-first-word">Gender: </span>{currentProfile.gender}</p>
+                    </div>
+                    <div className="swipe-profile-details">
+                      <p><span className="bold-first-word">Grade: </span>{currentProfile.grade}</p>
+                      <p><span className="bold-first-word">Relevant Coursework: </span>{currentProfile.relevant_courses}</p>
+                      <p><span className="bold-first-word">Fav Study Method: </span>{currentProfile.study_method}</p>
+                      <p><span className="bold-first-word">Study Tags: </span>
+                        {currentProfile.studyHabitTags.length > 0 ? (
+                          currentProfile.studyHabitTags.map((tag: string, index: number) => (
+                            <span key={index} className="tag">
+                              {formatEnum(tag)}
+                            </span>
+                          ))
+                        ) : (
+                          "No study tags specified."
+                        )}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </>
             </div>
           )}
-        </div>
-      );
-  };
 
-      export default SwipeProfiles;
+          <div className="swipe-buttons-container">
+            <button
+              onClick={handleBack}
+              disabled={currentProfileIndex === 0}
+              style={{ visibility: currentProfileIndex > 0 ? "visible" : "hidden" }}
+            >
+              Back
+            </button>
+
+            <div className="swipe-action-buttons">
+              <button onClick={() => { handleSwipe("Yes", currentProfile.id, !!currentProfile.studyGroupId); handleMessaging(); }}>
+                Match
+              </button>
+
+              <button onClick={() => handleSwipe("No", currentProfile.id, !!currentProfile.studyGroupId)}>
+                Skip
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="swipe-info">
+          <p>No more profiles to swipe on!</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SwipeProfiles;
