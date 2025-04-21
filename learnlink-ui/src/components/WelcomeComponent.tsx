613e1a615e32632f83ab2e59633fe9befa6654d2
nataliecrawford <crawfon@email.sc.edu>
2025-04-21 13:41:18 -0400

    adding more to the Why learnlink section of the website #404

--- learnlink-ui/src/components/WelcomeComponent.tsx
+++ learnlink-ui/src/components/WelcomeComponent.tsx
@@ -7,6 +7,11 @@ import studySmarter from '../images/study-smarter.png';
 import stayConnected from '../images/stay-connected.png';
 import weeklyScheduler from '../images/weekly-scheduler.png';
 import videoPlaceholder from '../images/video-placeholder.png';
+import groupsImage from '../images/study-group.png';
+import calculatorImage from '../images/blueCalculator.png';
+import profileImage from '../images/profile.png';
+import networkImage from '../images/network.png';
+import calendarInviteImage from '../images/calendar.png';
 import kennedy from '../images/profile pictures/kennedy-profile.png';
 import kelly from '../images/profile pictures/kelly-profile.png';
 import natalie from '../images/profile pictures/natalie-profile.png';
@@ -15,8 +20,14 @@ import yesha from '../images/profile pictures/yesha-profile.png';
 import studySmarterScreenshot from '../images/screenshots/Study-smarter-screenshot.png';
 import stayConnectedScreenshot from '../images/screenshots/Stay-connected-screenshot.png';
 import weeklySchedulerScreenshot from '../images/screenshots/weekly-scheduler-screenshot.png';
+import groupsPageScreenshot from '../images/screenshots/Groups-page.png';
+import resourcesScreenshot from '../images/screenshots/Resources.png';
+import profileScreenshot from '../images/screenshots/Profile.png';
+import calendarInviteScreenshot from '../images/screenshots/CalendarInvite.png';
+import networkScreenshot from '../images/screenshots/Network.png';
 
 import { useState } from 'react';
+import { group } from 'console';
 
 function Welcome() {
 
@@ -70,29 +81,65 @@ function Welcome() {
                 {/* Why LearnLink Section */}
                 <div className="infoSection">
                     <h1>Why LearnLink?</h1>
-                    <p> In large classes, it’s easy to feel lost or disconnected. LearnLink simplifies collaboration by connecting you with the right study partners, keeping conversations organized, and helping you track your academic progress.</p>
-                    <p> With a user-friendly interface and smart matching tools, LearnLink empowers students to build reliable study groups, share resources effortlessly, and stay on top of their goals throughout the semester.</p>
-
+                    <p> Tired of studying alone or struggling to find the right people to prep with? In large classes, it’s easy to feel lost or disconnected. LearnLink is your solution.</p>
+                    <p> Our website makes it easy to find and connect with like-minded students for more productive and collaborative study sessions.</p>
+                    <p> Whether you're looking for a one-on-one partner or a full-on study squad, you can browse and match with individual users or existing groups. 
+                        Once a request is accepted, you can start messaging immediately, share weekly schedules, and send calendar invites to stay on track together.</p>
                         <div className="whyBoxes">
                             <div className="whyBox" onClick={() => handleOpenModal(studySmarterScreenshot)}>
                                 <div className="hoverOverlay">Click to View Screenshot</div>
-                                <h2>Study Smarter</h2>
-                                <p>Find peers with similar schedules and goals.</p>
+                                <h2>Match & Connect</h2>
+                                <p>Send and receive match or join requests with individuals or groups based on shared classes, study habits, or goals.</p>
                                 <img src={studySmarter} alt="Study Smarter Icon" />
                             </div>
                             <div className="whyBox" onClick={() => handleOpenModal(stayConnectedScreenshot)}>
                                 <div className="hoverOverlay">Click to View Screenshot</div>
                                 <h2>Stay Connected</h2>
-                                <p>Easy messaging and notifications.</p>
+                                <p>Easy messaging and notifications. Chat in real time to plan sessions, ask questions, and stay in sync.</p>
                                 <img src={stayConnected} alt="Stay Connected Icon" />
                             </div>
                             <div className="whyBox" onClick={() => handleOpenModal(weeklySchedulerScreenshot)}>
                                 <div className="hoverOverlay">Click to View Screenshot</div>
                                 <h2>Weekly Scheduler</h2>
-                                <p>Find the best times for your group to study together.</p>
+                                <p>Share availability. Find the best times for your group to study together.</p>
                                 <img src={weeklyScheduler} alt="Weekly Scheduler Icon" />
                             </div>
+                            <div className="whyBox" onClick={() => handleOpenModal(groupsPageScreenshot)}>
+                                <div className="hoverOverlay">Click to View Screenshot</div>
+                                <h2>Groups Page</h2>
+                                <p>See all your current study groups at a glance. Click into each one to view/edit members and group profile information.</p>
+                                <img src={groupsImage} alt="Study Group Icon" />
+                            </div>
+                            <div className="whyBox" onClick={() => handleOpenModal(resourcesScreenshot)}>
+                                <div className="hoverOverlay">Click to View Screenshot</div>
+                                <h2>Resources Page</h2>
+                                <p>View some helpful studying tips/resources and access the websites grade calculator.</p>
+                                <img src={calculatorImage} alt="Resouces Icon" />
+                            </div>
+                            <div className="whyBox" onClick={() => handleOpenModal(profileScreenshot)}>
+                                <div className="hoverOverlay">Click to View Screenshot</div>
+                                <h2>Profile</h2>
+                                <p>Customize your profile with classes you're taking, your study preferences, and your ideal match factor.</p>
+                                <img src={profileImage} alt="Profile Icon" />
+                            </div>
+                            <div className="whyBox" onClick={() => handleOpenModal(calendarInviteScreenshot)}>
+                                <div className="hoverOverlay">Click to View Screenshot</div>
+                                <h2>Calendar Invites</h2>
+                                <p>Schedule upcoming study sessions, and send invites that sync to your calendar.</p>
+                                <img src={calendarInviteImage} alt="Calendar Invite Icon" />
+                            </div>
+                            <div className="whyBox" onClick={() => handleOpenModal(networkScreenshot)}>
+                                <div className="hoverOverlay">Click to View Screenshot</div>
+                                <h2>Network Page</h2>
+                                <p>See your full list of connections, revisit past matches, and start new one-on-one or group conversations with just a tap.</p>
+                                <img src={networkImage} alt="Network Icon" />
+                            </div>
                     </div>
+                    <p></p>
+                    <p> This app was built for students by students who know how challenging it can be to stay on top of coursework in large or fast-paced classes.
+                        From finding the right people to study with to coordinating your schedules, this app streamlines the entire process.
+                    </p>
+                    <p> Whether you're cramming for a final, tackling a group project, or just need a motivation boost, your next study buddy—or your entire support squad—is just a few clicks away.</p>
                 </div>
 
                 {/* Modal for Enlarged Screenshot */}
