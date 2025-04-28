8d2aa4b3881e5a2d7938652d98c929af5cd2b945
raej15 <sej15@email.sc.edu>
2025-04-27 22:21:17 -0400

    dmeo vid added

--- learnlink-ui/src/components/WelcomeComponent.tsx
+++ learnlink-ui/src/components/WelcomeComponent.tsx
@@ -83,7 +83,7 @@ function Welcome() {
                     <iframe
                         width="560"
                         height="315"
-                        src="https://www.youtube.com/embed/1bzXs333ww4"
+                        src="https://www.youtube.com/embed/TcrpOWsWAdw"
                         frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen
